b45478be35a80221980140533877351fbd5ce7f7
hrbrmstr <bob@rud.is>
2019-03-23 12:38:11 -0400

    testing output

--- R/functions.R
+++ R/functions.R
@@ -5,12 +5,27 @@
 #' @example /examples/example.R
 #' @export
 nmap <- function(...) {
+
+  td <- tempfile("out-")
+  dir.create(td)
+  on.exit(unlink(td, recursive=TRUE), add=TRUE)
+
   # convert the ... into a argument list
   arglist <- outsider::.arglist_get(...)
   # create an outsider object: describe the arguments and program
   otsdr <- outsider::.outsider_init(repo = 'hrbrmstr/om..nmap',
-                                    cmd = 'nmap',
+                                    cmd = 'nmap', wd = td,
                                     arglist = arglist)
   # run the command
-  outsider::.run(otsdr)
+  out <- outsider::.run(otsdr)
+
+  if (out) {
+    fils <- list.files(td, full.names = TRUE)
+    fils <- lapply(fils, readLines)
+    fils <- stats::setNames(fils, basename(fils))
+    return(fils)
+  } else {
+    return(out)
+  }
+
 }
